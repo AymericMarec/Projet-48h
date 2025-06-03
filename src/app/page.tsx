8df3d283b1bd5f93d8d9a55ad376eab876e58147
Diane-SDP <diane.sautereau@gmail.com>
2025-06-03 10:49:57 +0200

    restore homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import Image from 'next/image';
 import SearchStation from "@/component/searchStation"
 import FindMyPath from "@/component/findmyPath";
-import StatusStations from "@/component/statusStations";
 import '@/css/index.css';
 import { useEffect, useState } from 'react';
 import { RawStation } from '@/type/map';
@@ -32,9 +31,40 @@ export default function Home() {
 
   return (
     <div>
-      {/* <Map></Map> */}
-      <SearchStation />
-      <FindMyPath />
+    <Header/>
+    <div className="index-root">
+
+      <main className="index-main">
+        <div className="index-bg-overlay">
+          <Image src="/img/velo_main.png" alt="Vélos" fill priority className="index-bg-img" />
+        </div>
+        <div className="index-main-content">
+          <div className="index-hero-left">
+            <h1 className="index-title">Les vélos en libre service,<br />partout, tout le temps.</h1>
+          </div>
+          <div className="index-hero-right">
+            <div className="index-search-blocks">
+              <div className="index-search-station">
+                <div className="index-block-header">
+                  <h2 className="index-block-title">TROUVER MA STATION</h2>
+                </div>
+                <div className="index-block-body">
+                  <SearchStation />
+                </div>
+              </div>
+              <div className="index-search-path">
+                <div className="index-block-header">
+                  <h2 className="index-block-title">RECHERCHER MON ITINÉRAIRE</h2>
+                </div>
+                <div className="index-block-body">
+                  <FindMyPath />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
     </div>
   );
 }
